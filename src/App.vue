2ba50c22b1587dab47244e761cc940a6f641f16f
Melia-Ferreira <melia.ferreira@etud.univ-jfc.fr>
2023-02-24 09:11:22 +0100

    css header

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,9 @@ import Librairie from "./components/Librairie.vue";
 </template>
 
 <style>
+body {
+  /* background-image: url("./assets/fond étagère.jpg");
+  background-size: cover;
+  backdrop-filter: blur(1px); */
+}
 </style>
--- src/components/LibrairieHeader.vue
+++ src/components/LibrairieHeader.vue
@@ -1,11 +1,44 @@
 <template>
-  <h1>MA LIBRAIRIE</h1>
+  <div>
+  <h1><img src="../assets/iconelivre.jpg" align="absmiddle" alt= "Livre Icone">BIENVENUE DANS VOTRE LIBRAIRIE<img src="../assets/iconelivre.jpg" align="absmiddle" alt= "Livre Icone"></h1>
+  <h2>Achetez vos livres neufs en ligne aux meilleurs prix garantis parmi des millions de titres</h2>
+  </div>
+  <hr>
 </template>
 
 <style scoped>
-h1 {
-  margin: 40px 0 0;
-  color: blue;
-  text-align: center;
+
+div {
+  background-color: pink;
+  width: 1000px;
+  margin : 40px auto;
+  padding : 30px;
+  border-radius: 30px;
 }
+h1 {
+  color: white;
+  text-align: center;
+  font-family: 'Courier New', Courier, monospace;
+  font-weight:bold;
+  font-size: 250% ;
+}
+h2 {
+  color: whitesmoke;
+  text-align: center;
+  font-family: 'Courier New', Courier, monospace;
+  font-weight:bold;
+  font-size: 100% ;
+}
+
+hr{
+  height: 3px;
+  background-color: pink;
+  border: none;
+}
+
+img {
+  width : 60px;
+  height :60px;
+}
+
 </style>
